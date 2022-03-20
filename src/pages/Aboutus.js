ba98fadf9fb715acb4ccedecb1e21319526d4ed8
Adeelshaikh01 <adeelshaikh654@gmail.com>
2022-03-20 16:00:07 +0500

    changes in about us

--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -7,7 +7,7 @@ function Aboutus() {
   return (
     <Layout>
      <Box>
-       <Typography variant="h4" sx={{fontWeight:"bold"}}>About Us Page Changed!!!</Typography>
+       <Typography variant="h4" sx={{fontWeight:"bold",bgcolor:"red",py:12}}>About Us Page Changed!!!</Typography>
        <Link to="/">Home</Link>
      </Box>
     </Layout>
