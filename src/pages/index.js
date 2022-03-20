2a7afd054fb796eb28d283c296b6a6851e67c50e
Adeelshaikh01 <adeelshaikh654@gmail.com>
2022-03-20 15:42:43 +0500

    some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,9 +134,9 @@ const IndexPage = () => {
       <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
+        Congratulations 
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
+        <span style={headingAccentStyles}>— you just deployed first site to cloud </span>
         <span role="img" aria-label="Party popper emojis">
           🎉🎉🎉
         </span>
